07905304c5e428dcc2fd6bb7c0bc553d630e0929
Gabriel Brun <lydstyl@gmail.com>
2024-10-27 00:49:19 +0200

    Create a navigation for cv and portfolio pages.

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,25 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+// import { Controller, Get } from '@nestjs/common';
+// import { AppService } from './app.service';
+
+// @Controller()
+// export class AppController {
+//   constructor(private readonly appService: AppService) {}
+
+//   @Get()
+//   getHello(): string {
+//     return this.appService.getHello();
+//   }
+// }
+
+import { Controller, Get, Render } from '@nestjs/common';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Render('cv') // cela indique de rendre le fichier `cv.ejs`
+  getCV() {
+    return {}; // on peut passer des données ici si nécessaire
   }
 }
+
+// this.appService service ?
